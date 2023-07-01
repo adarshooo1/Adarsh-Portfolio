8e79a58d7d7535d0c5b19a7af77005fe4e45ef1f
Adarsh Singh <adarshsinghak001@gmail.com>
2023-07-01 19:04:47 +0530

    Making my portfolio. Final Changes attempt 8 now proceeding to deploy at vercel and works well

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,11 +1,12 @@
+import { sanityClient } from "../adarsh-portfolio-backend";
 import { PageInfo } from "../typings";
+import { groq } from "next-sanity";
+
+const query = groq`
+*[_type == "pageInfo"][0]
+`;
+
 export const fetchPageInfo = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
-  );
-
-  const data = await res.json();
-  const pageInfo: PageInfo = data.pageInfo;
-
+  const pageInfo: PageInfo = await sanityClient.fetch(query);
   return pageInfo;
 };
--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,10 +1,15 @@
+import { groq } from "next-sanity";
+import { sanityClient } from "../adarsh-portfolio-backend";
 import { Project } from "../typings";
 
-export const fetchProjects = async() =>{
-    const res = await fetch (`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`);
-    
-    const data = await res.json()
-    const projects : Project[] = data.projects;
+const query = groq`
+*[_type == "project"]{
+  ...,
+  technologies[]->
+    }
+`;
 
-    return projects;
-}
+export const fetchProjects = async () => {
+  const projects: Project[] = await sanityClient.fetch(query);
+  return projects;
+};
