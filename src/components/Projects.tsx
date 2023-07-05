e58cd64d0d394599f07fb94dafc8dc8a438b003f
Adarsh Singh <adarshsinghak001@gmail.com>
2023-07-05 16:45:31 +0530

    In Portfolio give styling to the Project Section Completed

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,22 +10,38 @@ type Props = {
 
 function Projects({ projects }: Props) {
   return (
+    // Parent Box
     <motion.div
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="xs:h-[140vh] sm:h-[150vh] relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-center mx-auto items-center z-0"
+      //                          Universal                                                                                        |  Extra Large Screens   |   Larger Screen    |   Medium Screen   |    Smaller Screen    |    Mobile Screen     |
+      className="h-screen relative flex overflow-hidden flex-col max-w-full justify-start text-center m-auto items-center z-0 pb-5       xl:h-[160vh]            lg:h-[150vh]         md:h-[120vh]         sm:h-[110vh]           xs:h-[140vh]"
     >
+
+            {/* ====================================================================================================== */}
+
+      {/*Main Header */}
       <h3 className="absolute top-24 uppercase text-2xl text-white">
         <span className="tracking-[20px]">project</span>s
       </h3>
 
+
+            {/* ====================================================================================================== */}
+
+      {/* Child Which has project image , Project Skill , project Discription */}
       <div className="reltive w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-zinc-700/20 scrollbar-thumb-[#FB923C] scrollbar-thin">
         {projects.map((project, i) => (
           <div
             key={project._id}
-            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-5 md:p-20 h-screen xs:h-screen xs:mt-40"
+            //Align image with all content for all the screen sizes                                              | Extra large Screen | Large Screens | Medium Screens | Smaller Screens | Mobile Screens
+            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 justify-center items-center p-5      xl:mt-36            lg:mt-40        md:mt-40          sm:mt-40         xs:mt-40 "
           >
+
+            {/* ====================================================================================================== */}
+
+            {/* Image Container */}
+            {/*  */}
             <motion.img
               initial={{
                 y: -300,
@@ -36,13 +52,18 @@ function Projects({ projects }: Props) {
               viewport={{ once: true }}
               src={urlFor(project?.image).url()}
               alt={project?.title}
-              className="xl:w-[600px] mt-10"
+              //Setting Image Width according to the screen sizes
+              className="xl:w-[800px] lg:w-[600px] md:w-[500px] sm:w-[400px] xs:w-[350px]"
             />
 
+
+            {/* ====================================================================================================== */}
+
+            {/* Project Title */}
             <div className="space-y-10 px-0 md:px-5 mx-w-7xl">
-              <h4 className="sm:text-md xs:text-sm md:text-3xl text-4xl font-semibold text-center uppercase">
+              <h4 className="xs:text-sm sm:text-lg  md:text-xl lg:text-2xl text-4xl font-semibold text-center uppercase">
                 Project {i + 1} / 3:{" "}
-                <span className="uppercase font-bold">
+                <span className="uppercase font-bold xs:text-sm sm:text-lg md:text-xl lg:text-2xl text-4xl">
                   <ProjectTypewriter
                     words={[project?.title]}
                     loop
@@ -52,6 +73,9 @@ function Projects({ projects }: Props) {
                 </span>
               </h4>
 
+            {/* ====================================================================================================== */}
+
+              {/*  Technology used for make projects */}
               <div className="flex items-center space-x-2 justify-center">
                 {project?.technologies.map((technology) => (
                   <img
@@ -63,6 +87,9 @@ function Projects({ projects }: Props) {
                 ))}
               </div>
 
+            {/* ====================================================================================================== */}
+
+              {/* Project Discription */}
               <p className="md:text-md sm:text-sm lg:text-lg xl:text-xl xl:text-center text-left pb-20">
                 {project?.summary}
               </p>
@@ -71,6 +98,9 @@ function Projects({ projects }: Props) {
         ))}
       </div>
 
+            {/* ====================================================================================================== */}
+
+      {/* background Orange Line which is Diagonally placed to Beautify */}
       <div className="w-full absolute top-[30%] bg-[#FB923C]/20 left-0 h-[400px] -skew-y-12"></div>
     </motion.div>
   );
