92624ae0c215c42ca6996b4699982e5f103325c4
Adarsh Singh <adarshsinghak001@gmail.com>
2023-11-08 01:34:03 +0530

    Skills Section SEO Practices by adding alt attribute to each

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -33,12 +33,12 @@ function Skill({ skill, directionLeft }: Props) {
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
         src={urlFor(skill.image).url()}
+        alt={skill.title}
         className="rounded-full border border-gray-500 object-cover w-24 h-24    xl:w-24 xl:h-24      lg:w-[5rem] lg:h-[5rem]     md:w-20 md:h-20    sm:w-16 sm:h-16    xs:w-12 xs:h-12      filter group-hover:grayscale transition duration-300 ease-in-out"
       />
 
       {isHovered && (
-
-        // For maing Gray effect over skills
+        // For making Gray effect over skills while hover
         <div className="absolute opacity-90 bg-gray-500 h-24 w-24 rounded-full z-0             xl:w-24 xl:h-24      lg:w-[5rem] lg:h-[5rem]    md:w-20 md:h-20      sm:w-16 sm:h-16     xs:w-12 xs:h-12">
           <div className="flex items-center justify-center h-full">
             <p className="font-bold text-black opacity-100     xl:text-3xl    sm:text-lg">
